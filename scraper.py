1bca5c96e6b8361d644c72716f80cde79dad61cf
Duncan Parkes <duncan.parkes@gmail.com>
2015-05-18 13:24:34 +0100

    First go at scraping current politicians.

---
+++ scraper.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# This is a template for a Python scraper on morph.io (https://morph.io)
+# including some code snippets below that you should find helpful
+
+# import scraperwiki
+import requests
+import lxml.html
+
+# source_url = 'http://www.assemblee-nationale.mg/?page_id=6549'
+source_url = 'http://www.assemblee-nationale.mg/?page_id=5217'
+response = requests.get(source_url)
+
+root = lxml.html.fromstring(response.text)
+
+lis = root.get_element_by_id('deputes').cssselect('li')
+
+data = []
+
+for li in lis:
+    member = {}
+
+    member['details_url'] = li.cssselect('a')[0].get('href')
+
+    img = li.cssselect('img')[0]
+    member['image'] = img.get('src')
+    member['name'] = img.get('title')
+
+    member_resp = requests.get(member['details_url'])
+    member_root = lxml.html.fromstring(member_resp.text)
+
+    mfiche = member_root.cssselect('.mfiche')
+    pairs = [(x.text, x.tail) for x in mfiche[0]]
+
+    for key, val in pairs:
+        key = key.strip() if key else ''
+        val = val.strip() if val else ''
+
+        if not key or not val:
+            continue
+        if key == u'Nom:':
+            member['name'] = val
+        elif key == u'District:':
+            member['district'] = val
+        elif key == u'Région:':
+            member['region'] = val
+        elif key == u'Parti:':
+            member['party'] = val
+        elif key == u'Fonction parlementaire:':
+            member['executive'] = val
+        elif key == u'Age:':
+            # I see no point in us storing age, that'll just go out of date
+            continue
+# Things we're not handling yet:
+# 'Membre des commissions:'
+
+# These appear commented out in the source.
+# 'Adresse:':
+# 'Date de naissance:':
+# 'Contacts t\xe9l\xe9phonique:'
+# 'Province:'
+# 'R\xe9gion d'origine:'
+        else:
+            import pdb;pdb.set_trace()
+
+    data.append(member)
+
+scraperwiki.sqlite.save(unique_keys=['name'], data=data)
